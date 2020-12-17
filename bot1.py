c6154ea2938f6724afd68eb16e88dcb7ee0c5ff6
Vera Burdina <veraburdi999@gmail.com>
2020-12-17 12:01:37 +0300

    add language selection and handling wrong inputs

--- bot1.py
+++ bot1.py
@@ -12,36 +12,71 @@ def image_to_byte_array(image:Image):
 
 bot = telebot.TeleBot('1200755615:AAHs_ArTkrn-NXJ7TJIVb94TRLPqjr3RRXU');
 
-
 @bot.message_handler(content_types=['text', 'photo'])
 def start(message):
     if message.text == '/start':
-        bot.send_message(message.from_user.id, "Скинь фоточку, которую хочешь застайлить!");
-        bot.register_next_step_handler(message, get_content); #следующий шаг – функция get_content
+        keyboard1 = telebot.types.ReplyKeyboardMarkup()
+        keyboard1.row('english', 'russian')
+        bot.send_message(message.from_user.id, "Please, choose language./Пожалуйста, выберите язык.", reply_markup = keyboard1);
+        bot.register_next_step_handler(message, choose_lang); #следующий шаг – функция get_content
     else:
-        bot.send_message(message.from_user.id, 'Напиши /start');
+        bot.send_message(message.from_user.id, 'Write/Напишите   /start');
+        bot.register_next_step_handler(message, start);
 
-def get_content(message): #получаем контент
+def choose_lang(message):
+    if message.text == 'russian':
+        bot.send_message(message.from_user.id, "Вы выбрали русский! Пожалуйста, пришлите фотографию, которой вы бы хотели придать стиля!:)");
+        bot.register_next_step_handler(message, get_content_russian);
+    if message.text == 'english':
+        bot.send_message(message.from_user.id, "You have chosen English! Please send a photo that you would like to style! :)");
+        bot.register_next_step_handler(message, get_content_english);
+    if message.text != 'russian' and message.text != 'english':
+        bot.send_message(message.from_user.id, "You sent me something wrong ... Please, choose language./Пожалуйста, выберите язык.", reply_markup = keyboard1);
+        bot.register_next_step_handler(message, choose_lang);
 
-    try:
 
 
+
+def get_content_russian(message): #получаем контент
+    if message.photo == None:
+        bot.send_message(message.from_user.id, "Вы прислали мне что-то не то... Пожалуйста, пришлите фотографию, которой вы бы хотели придать стиля!:)");
+        bot.register_next_step_handler(message, get_content_russian);
+    else:
         file_info = bot.get_file(message.photo[0].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
         scr1 = 'content/'+ str(message.from_user.id) + '.png'
         with open(scr1, 'wb') as new_file:
            new_file.write(downloaded_file)
-        bot.reply_to(message, "Супер! Добавлено") 
+        bot.reply_to(message, "Супер! Добавлено")
+        bot.send_message(message.from_user.id, 'Теперь скинь фоточку стиля!');
+        bot.register_next_step_handler(message, get_style_russian);
+       
 
-    except Exception as e:
-        bot.reply_to(message,e )
-        
-    bot.send_message(message.from_user.id, 'Теперь скинь фоточку стиля!');
-    bot.register_next_step_handler(message, get_style);
 
-def get_style(message): #получаем стиль
+def get_content_english(message): #получаем контент
+
+    if message.photo == None:
+        bot.send_message(message.from_user.id, "You sent me something wrong ... Please send me a photo you would like to style! :)");
+        bot.register_next_step_handler(message, get_content_english);
+    else:
+        file_info = bot.get_file(message.photo[0].file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        scr1 = 'content/'+ str(message.from_user.id) + '.png'
+        with open(scr1, 'wb') as new_file:
+           new_file.write(downloaded_file)
+        bot.reply_to(message, "Great! Added")
+        bot.send_message(message.from_user.id, 'Now send me a photo of the style!');
+        bot.register_next_step_handler(message, get_style_english);
+
+
+
+def get_style_russian(message): #получаем стиль
     global scr1, scr2
-    try:
+    if message.photo == None:
+        bot.send_message(message.from_user.id, "Вы прислали мне что-то не то... Пожалуйста, пришлите фотографию стиля! ");
+        bot.register_next_step_handler(message, get_style_russian);
+
+    else:
         file_info = bot.get_file(message.photo[0].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
 
@@ -49,15 +84,35 @@ def get_style(message): #получаем стиль
         with open(scr2, 'wb') as new_file:
            new_file.write(downloaded_file)
         bot.reply_to(message, "Отлично, и это добавлено")
-        bot.send_message(message.from_user.id, "Теперь жди где-то секунд 30 ок да")
+        bot.send_message(message.from_user.id, "Теперь пожалуйста подождите где-то секунд 30")
 
-    except Exception as e:
-        bot.reply_to(message,e )
-    print(scr2)
-    output_name = main_func('content/'+ str(message.from_user.id) + '.png', 'style/' + str(message.from_user.id) + '.png')
-    bot.send_photo(message.from_user.id, open(output_name, 'rb'));
+        output_name = main_func('content/'+ str(message.from_user.id) + '.png', 'style/' + str(message.from_user.id) + '.png')
+        bot.send_photo(message.from_user.id, open(output_name, 'rb'));
 
-    bot.send_message(message.from_user.id, "Хочешь еще разок? Жми ---> /start");
+        bot.send_message(message.from_user.id, "Хочешь еще разок? Жми ---> /start");
+        bot.register_next_step_handler(message, start);
+
+def get_style_english(message): #получаем стиль
+    global scr1, scr2
+    if message.photo == None:
+        bot.send_message(message.from_user.id, "You sent me something wrong ... Please send a photo of the style! ");
+        bot.register_next_step_handler(message, get_style_english);
+
+    else:
+        file_info = bot.get_file(message.photo[0].file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        scr2 = 'style/' + str(message.from_user.id) + '.png'
+        with open(scr2, 'wb') as new_file:
+           new_file.write(downloaded_file)
+        bot.reply_to(message, "Great! Added")
+        bot.send_message(message.from_user.id, "Now please wait about 30 seconds")
+
+        output_name = main_func('content/'+ str(message.from_user.id) + '.png', 'style/' + str(message.from_user.id) + '.png')
+        bot.send_photo(message.from_user.id, open(output_name, 'rb'));
+
+        bot.send_message(message.from_user.id, "One more time? Press ---> /start");
+        bot.register_next_step_handler(message, start);
 
 
 
